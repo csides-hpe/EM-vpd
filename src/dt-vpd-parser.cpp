3730558bee8febe49344a68a63ccec24f6a1e5bf
Christopher Sides <99367432+csides-hpe@users.noreply.github.com>
2024-10-10 16:05:32 -0500

    Update dt-vpd-parser.cpp
    Got rid of startup() -- example code for async server in sdbusplus repo things this way, and relying on startup() introduces some scope issues that don't seem worth adding complexity over.

--- src/dt-vpd-parser.cpp
+++ src/dt-vpd-parser.cpp
@@ -1,19 +1,17 @@
 #include "machinecontext.hpp"
 
-auto startup(sdbusplus::async::context& ctx) -> sdbusplus::async::task<>
- {
-    sdbusplus::server::manager_t manager{ctx, MachineContext::ReqDBusPath};
-
-    ctx.request_name(MachineContext::ReqDBusInterface);
-    co_return;
- }
-
 int main()
 {
     sdbusplus::async::context ctx;
+	
+    sdbusplus::server::manager_t manager{ctx, MachineContext::ReqDBusPath};
     MachineContext c{ctx, MachineContext::ReqDBusPath};
- 
-    ctx.spawn(startup(ctx));
+	
+    ctx.spawn([](sdbusplus::async::context& ctx) -> sdbusplus::async::task<> {
+            ctx.request_name(MachineContext::ReqDBusInterface);
+            co_return;
+        }(ctx));
+
     ctx.run();
 
     return 0;
